3c03e2aa4d2236c66fe01c35a946adb951b7babf
Jems <jamakhanak@gmail.com>
2024-01-17 17:37:02 +0545

    FE-Q3Q7s5jV-categoryHovere: remove the style from categories and make the component styles and import to categories

---
+++ src/component/category/styles.js
@@ -0,0 +1,71 @@
+export const styles = {
+  categoryCardStyles: {
+    height: "20rem",
+    width: "15rem",
+    borderRadius: "1rem",
+    marginTop: "1rem",
+    paddingBottom: "5rem",
+    marginLeft: "25rem",
+    cursor: "pointer",
+  },
+
+  listItemCategory: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+
+  typographyCategory: {
+    fontSize: "12px",
+  },
+
+  subCategoryGrid: {
+    display: "flex",
+  },
+
+  subCategoryCard: {
+    height: "24rem",
+    width: "15rem",
+    borderRadius: "1rem",
+    marginTop: "1rem",
+    paddingBottom: "1rem",
+    marginLeft: "10rem",
+    cursor: "pointer",
+  },
+
+  listItemSubcategory: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+
+  typographySubcategory: {
+    fontSize: "12px",
+    width: "100%",
+  },
+
+  productsCard: {
+    height: "23rem",
+    width: "35rem",
+    borderRadius: "1rem",
+    marginTop: "1rem",
+    padding: "1rem",
+    marginLeft: "0rem",
+    cursor: "pointer",
+    display: "flex",
+    flexWrap: "wrap",
+    overflow: "auto",
+    objectFit: "contain",
+  },
+
+  imageControl: {
+    height: "50px",
+    width: "50px",
+    borderRadius: "10rem",
+    padding: "1rem 2rem 1rem 2rem",
+  },
+
+  imageTypography: {
+    fontSize: "12px",
+    textAlign: "center",
+    padding: "0rem 1rem 1rem 1rem",
+  },
+};
