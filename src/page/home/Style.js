33e839606dcb8f07f12331c369044a6f09209be6
Jems <jamakhanak@gmail.com>
2024-01-17 17:37:02 +0545

    FE-Q3Q7s5jV-categoryHovere: remove the style from categories and make the component styles and import to categories

---
+++ src/page/home/Style.js
@@ -0,0 +1,11 @@
+export const styles = {
+  categoryCardStyles: {
+    height: "20rem",
+    width: "15rem",
+    borderRadius: "1rem",
+    marginTop: "1rem",
+    paddingBottom: "5rem",
+    marginLeft: "25rem",
+    cursor: "pointer",
+  },
+};
