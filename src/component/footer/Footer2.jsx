e559aad5aefa6f78f3adc44770fc31b81b66f1fb
Jems <jamakhanak@gmail.com>
2024-01-19 14:20:03 +0545

    FE-weduVdBd-footer: add the image and icon in the footer2

--- src/component/footer/Footer2.jsx
+++ src/component/footer/Footer2.jsx
@@ -3,6 +3,9 @@ import { Link, useNavigate } from "react-router-dom";
 
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 import { styles } from "./styles";
 
@@ -17,48 +20,42 @@ const Footer2 = () => {
           Customer Care
         </Typography>
 
-        <Typography style={{fontSize: '14px'}}>Help Center</Typography>
+        <Typography style={{ fontSize: "14px" }}>Help Center</Typography>
 
-        <Typography style={{ fontSize: "14px" }}>
-          How to Buy
+        <Typography style={{ fontSize: "14px" }}>How to Buy</Typography>
+
+        <Typography style={{ fontSize: "14px" }}>Returns & Refunds</Typography>
+
+        <Typography style={{ fontSize: "14px", marginBottom: "1rem" }}>
+          Contact Us
         </Typography>
 
-        <Typography style={{fontSize: '14px'}}>
-          Returns & Refunds
-        </Typography>
-
-        <Typography style={{fontSize: '14px',marginBottom: '1rem'}}>Contact Us</Typography>
-
-        <Typography variant="h2" style={{fontSize: '18px',color: 'red',marginBottom: '1rem'}}>
+        <Typography
+          variant="h2"
+          style={{ fontSize: "18px", color: "red", marginBottom: "1rem" }}
+        >
           Earn with Daraz
         </Typography>
 
-        <Typography style={{fontSize: '14px'}}>Daraz University</Typography>
+        <Typography style={{ fontSize: "14px" }}>Daraz University</Typography>
 
         <Typography style={{ fontSize: "12px" }}>Sell on Daraz</Typography>
 
         <Typography style={{ fontSize: "12px" }}>Code of Conduct</Typography>
       </Grid>
 
-      <Grid item xs={3} style={{  padding: "5rem" }}>
-        <Typography
-          variant="h4"
-          style={{  marginBottom: "1rem" ,color: 'red'}}
-        >
+      <Grid item xs={3} style={{ padding: "5rem" }}>
+        <Typography variant="h4" style={{ marginBottom: "1rem", color: "red" }}>
           Daraz
         </Typography>
 
-        <Typography style={{fontSize: '14px'}}>About Daraz</Typography>
+        <Typography style={{ fontSize: "14px" }}>About Daraz</Typography>
 
         <Typography style={{ fontSize: "14px" }}>Careers</Typography>
 
-        <Typography style={{ fontSize: "14px"}}>
-          Daraz Blog
-        </Typography>
+        <Typography style={{ fontSize: "14px" }}>Daraz Blog</Typography>
 
-        <Typography style={{ fontSize: "14px"}}>
-          Terms & Conditions
-        </Typography>
+        <Typography style={{ fontSize: "14px" }}>Terms & Conditions</Typography>
 
         <Typography style={{ fontSize: "14px" }}>Privacy Policy</Typography>
 
@@ -79,80 +76,80 @@ const Footer2 = () => {
         <Typography style={{ fontSize: "14px" }}>Realme GT Neo 3</Typography>
       </Grid>
 
-      <Grid item container xs={3} >
-      <Grid item xs={12} style={{ padding: "5rem" }}>
-        <Typography
-          variant="h2"
-          style={{ fontSize: "18px", marginBottom: "1rem", color: "red" }}
-        >
-          Daraz International
-        </Typography>
+      <Grid item container xs={3}>
+        <Grid item xs={12} style={{ padding: "5rem" }}>
+          <Typography
+            variant="h2"
+            style={{ fontSize: "18px", marginBottom: "1rem", color: "red" }}
+          >
+            Daraz International
+          </Typography>
 
-        <Typography style={{fontSize: '14px'}}> Pakistan</Typography>
+          <Typography style={{ fontSize: "14px" }}> Pakistan</Typography>
 
-        <Typography style={{ fontSize: "14px" }}>
-         Bangladesh
-        </Typography>
+          <Typography style={{ fontSize: "14px" }}>Bangladesh</Typography>
 
-        <Typography style={{fontSize: '14px'}}>
-          Sir Lanka
-        </Typography>
+          <Typography style={{ fontSize: "14px" }}>Sir Lanka</Typography>
 
-        <Typography style={{fontSize: '14px'}}>Myanmar</Typography>
+          <Typography style={{ fontSize: "14px" }}>Myanmar</Typography>
 
-        <Typography style={{fontSize: '14px'}}>
-         Nepal
-        </Typography>
+          <Typography style={{ fontSize: "14px" }}>Nepal</Typography>
 
-        <Typography style={{fontSize: '18px',marginTop:'1rem',color: 'red'}}>Payment Method</Typography>
+          <Typography
+            style={{ fontSize: "18px", marginTop: "1rem", color: "red" }}
+          >
+            Payment Method
+          </Typography>
 
-        <Typography style={{fontSize: '18px',marginTop:'1rem',color: 'red'}}>Verified By</Typography>
+          <Typography style={{ marginTop: "1rem" }}>
+            <img src="/esewaimg.png" style={{ height: "20px" ,marginRight: '5px'}} />
+            <img src="/visaimg.png" style={{ height: "20px" ,marginRight: '10px'}} />
+            <img src="/imepayimg.png" style={{ height: "20px" ,marginRight: '10px'}} />
+            <img src="/mastercardimg.png" style={{ height: "20px" }} />
+          </Typography>
 
+          <Typography
+            style={{ fontSize: "18px", marginTop: "1rem", color: "red" }}
+          >
+            Verified By
+          </Typography>
+
+          <Typography style={{ marginTop: "1rem" }}>
+            <img src="/pciimg.png" style={{ height: "40px",width:'100px' ,marginRight: '5px'}} />
+          </Typography>
+
+        </Grid>
       </Grid>
-      </Grid>
 
-
-      <Grid item xs={3} style={{  padding: "5rem" }}>
-        <Typography
-          variant="h4"
-          style={{  marginBottom: "1rem" }}
-        >
-          Daraz
+      <Grid item xs={3} style={{ padding: "5rem" }}>
+        <Typography variant="h4" style={{ marginBottom: "1rem" ,color: 'red'}}>
+          Exclusive Deals and Offers!
         </Typography>
 
-        <Typography style={styles.everyTypography}>About Daraz</Typography>
+        <Typography style={{ color: "red" }}>
+        <img src="darazicon.png" style={{height: '30px',marginRight: '20px'}}/>
+          Happy Shopping</Typography>
 
-        <Typography style={{ fontSize: "12px" }}>Careers</Typography>
+        <Typography style={{ fontSize: "12px", marginBottom: "1rem" ,marginLeft: '50px'}}>
+         
+          Download App
+        </Typography>
 
-        <Typography style={{ fontSize: "12px", marginBottom: "1rem" }}>
-          Daraz Blog
+        <Typography
+          style={{ fontSize: "18px", marginBottom: "20px", color: "red" }}
+        >
+          Follow Us
         </Typography>
 
         <Typography style={{ fontSize: "18px", marginBottom: "1rem" }}>
-          Terms & Conditions
+          <InstagramIcon />
+          <FacebookIcon />
+          <YouTubeIcon />
         </Typography>
-
-        <Typography style={{ fontSize: "12px" }}>Privacy Policy</Typography>
-
-        <Typography style={{ fontSize: "12px" }}>Digital Payments</Typography>
-
-        <Typography style={{ fontSize: "12px" }}>
-          Daraz Customer University
-        </Typography>
-
-        <Typography style={{ fontSize: "12px" }}>
-          Daraz Affiliate Program
-        </Typography>
-
-        <Typography style={{ fontSize: "12px" }}>Review & Win</Typography>
-
-        <Typography style={{ fontSize: "12px" }}>Meet the winners</Typography>
-
-        <Typography style={{ fontSize: "12px" }}>Realme GT Neo 3</Typography>
       </Grid>
     </Grid>
   );
 };
 
 export default Footer2;
-export{Footer2};
+export { Footer2 };
