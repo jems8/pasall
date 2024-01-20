27e07cdc35af632f21e265a3a746fb1695460c71
Jems <jamakhanak@gmail.com>
2024-01-20 16:12:09 +0545

    FE-8idgmiOa-navigate: change the format make array of object and map it to take products

--- src/component/navigation/Navigation.js
+++ src/component/navigation/Navigation.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import  {Link}  from "react-router-dom";
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-import  {Link}  from "react-router-dom";
+import { style } from "./styles";
 
-import { style } from "./style";
+const products = [
+    { name: "Mart", imageSrc: "martimg.png" },
+    { name: "Beauty", imageSrc: "beautyimg.png" },
+    { name: "Home & Decor", imageSrc: "homeimg.png" },
+    { name: "Proudly Nepali", imageSrc: "proudlynepaliimg.png" },
+    { name: "Fashion", imageSrc: "fashionimg.png" },
+    { name: "New on Daraz", imageSrc: "newondarazimg.png" },
+    { name: "Add to Card", imageSrc: "addtocardimg.gif" },
+    { name: "Everyday Low Price", imageSrc: "everydaylowpriceimg.png" },
+    { name: "Electronic Bazaar", imageSrc: "electronicbazzarimg.png" },
+    { name: "Global Collection", imageSrc: "globalcollectionimg.png" },
+  ];
 
 const Navigation = () => {
+
   const [hoveredProduct, setHoveredProduct] = useState(null);
 
   const handleMouseEnter = (product) => {
@@ -23,88 +36,17 @@ const Navigation = () => {
   };
 
   return (
-    <Box
-      style={style.navigationmainBox}
-    >
-      <ProductBox
-        product="Mart"
-        imageSrc="martimg.png"
-        isHovered={isProductHovered("Mart")}
-        onMouseEnter={() => handleMouseEnter("Mart")}
-        onMouseLeave={handleMouseLeave}
-      />
-
-      <ProductBox
-        product="Beauty"
-        imageSrc="beautyimg.png"
-        isHovered={isProductHovered("Beauty")}
-        onMouseEnter={() => handleMouseEnter("Beauty")}
-        onMouseLeave={handleMouseLeave}
-      />
-
-      <ProductBox
-        product="Home & Decor"
-        imageSrc="homeimg.png"
-        isHovered={isProductHovered("Home & Decor")}
-        onMouseEnter={() => handleMouseEnter("Home & Decor")}
-        onMouseLeave={handleMouseLeave}
-      />
-
-      <ProductBox
-        product="Proudly Nepali"
-        imageSrc="proudlynepaliimg.png"
-        isHovered={isProductHovered("Proudly Nepali")}
-        onMouseEnter={() => handleMouseEnter("Proudly Nepali")}
-        onMouseLeave={handleMouseLeave}
-      />
-
-      <ProductBox
-        product="Fashion"
-        imageSrc="fashionimg.png"
-        isHovered={isProductHovered("Fashion")}
-        onMouseEnter={() => handleMouseEnter("Fashion")}
-        onMouseLeave={handleMouseLeave}
-      />
-
-      <ProductBox
-        product="New on Daraz"
-        imageSrc="newondarazimg.png"
-        isHovered={isProductHovered("New on Daraz")}
-        onMouseEnter={() => handleMouseEnter("New on Daraz")}
-        onMouseLeave={handleMouseLeave}
-      />
-
+    <Box style={style.navigationMainBox}>
+      {products.map((product) => (
         <ProductBox
-          product=" Add to Card"
-          imageSrc="addtocardimg.gif"
-          isHovered={isProductHovered("Add to Card")}
-          onMouseEnter={() => handleMouseEnter("Add to Card")}
+          key={product.name}
+          product={product.name}
+          imageSrc={product.imageSrc}
+          isHovered={isProductHovered(product.name)}
+          onMouseEnter={() => handleMouseEnter(product.name)}
           onMouseLeave={handleMouseLeave}
         />
-
-      <ProductBox
-        product="Everyday Low Price"
-        imageSrc="everydaylowpriceimg.png"
-        isHovered={isProductHovered("Everyday Low Price")}
-        onMouseEnter={() => handleMouseEnter("Everyday Low Price")}
-        onMouseLeave={handleMouseLeave}
-      />
-
-      <ProductBox
-        product="Electronic Bazaar"
-        imageSrc="electronicbazzarimg.png"
-        isHovered={isProductHovered("Electronic Bazaar")}
-        onMouseEnter={() => handleMouseEnter("Electronic Bazaar")}
-        onMouseLeave={handleMouseLeave}
-      />
-
-      <ProductBox
-        product=" Global Collection"
-        imageSrc="globalcollectionimg.png"
-        isHovered={isProductHovered("Global Collection")}
-        onMouseEnter={() => handleMouseEnter("Global Collection")}
-        onMouseLeave={handleMouseLeave}
-      />
+      ))}
     </Box>
   );
 };
--- src/component/navigation/styles.js
+++ src/component/navigation/styles.js
@@ -1,21 +1,26 @@
 export const style = {
-  navigationmainBox: {
+
+  navigationMainBox: {
     display: "flex",
     marginLeft: "17rem",
     width: "80rem",
     marginTop: "2rem",
   },
+
   navigationProductBox: {
     margin: "2rem",
     width: "4rem",
     alignItems: "center",
   },
+
   navigationLink: {
     textDecoration: "none",
   },
+
   navigationImage: {
     height: "6rem",
   },
+
   navigationTypography: {
     textAlign: "center",
     marginTop: "1rem",
